0139f47301d5e7d6a2a5adf91d1a32e06963f46b
ymushet <18755772+ymushet@users.noreply.github.com>
2020-09-18 22:36:17 +0300

    vowel_army added

--- alphabet_war.py
+++ alphabet_war.py
@@ -1,5 +1,7 @@
-left_side = {'w': 4, 'p': 3, 'b': 2, 's': 1}
-right_side = {'m': 4, 'q': 3, 'd': 2, 'z': 1}
+left_side = {'w': 4, 'p': 3, 'b': 2, 's': 1, 'y': 1}
+right_side = {'m': 4, 'q': 3, 'd': 2, 'z': 1, 'y': 3}
+# We will consider 'y' not a vowel letter https://simple.wikipedia.org/wiki/Vowel
+vowel_side = {'a': 1, 'e': 1, 'i' : 1, 'o': 1, 'u': 1}
 
 def find_letters_values(letters: list, values : dict) -> int:
     res = 0
@@ -10,32 +12,39 @@ def find_letters_values(letters: list, values : dict) -> int:
 
 def alphabet_war(text : str) -> str:
     """
-    >>> fight('')
+    >>> alphabet_war('')
     "Let's fight again!"
-    >>> fight('abracadabra')
-    'Left side wins!'
-    >>> fight('z')
+    >>> alphabet_war('abracadabra')
+    'Vowel side wins!'
+    >>> alphabet_war('z')
     'Right side wins!'
-    >>> fight('zdqmwpbs')
+    >>> alphabet_war('zdqmwpbs')
     "Let's fight again!"
-    >>> fight('zzzzs')
+    >>> alphabet_war('zzzzs')
     'Right side wins!'
-    >>> fight('wwwwwwz')
+    >>> alphabet_war('wwwwwwz')
     'Left side wins!'
+    >>> alphabet_war('u')
+    'Vowel side wins!'
+    >>> alphabet_war('aemmm')
+    'Right side wins!'
     """
 
     letters = list(text)
     right = find_letters_values(letters, right_side)
     left = find_letters_values(letters, left_side)
-    if right > left:
+    vowel = find_letters_values(letters, vowel_side)
+    if right > left and right > vowel:
         return 'Right side wins!'
-    elif left > right:
+    elif left > right and left > vowel:
         return 'Left side wins!'
-    print(left, right)
+    elif vowel > left and vowel > right:
+        return 'Vowel side wins!'
+
     return 'Let\'s fight again!'
 
 if __name__ == "__main__":
     """
     See https://www.codewars.com/kata/alphabet-war
     """
-    print(alphabet_war('smzwszfbsgsqbzdwfcesqmwsqqep'))
+    print(alphabet_war('u'))
